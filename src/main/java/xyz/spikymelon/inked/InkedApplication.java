a3c85c648db8a8d33b753b1fca3f242219260cd4
Introvert Hedgehog <larby2409@gmail.com>
2023-02-21 11:46:49 +0800

    init

---
+++ src/main/java/xyz/spikymelon/inked/InkedApplication.java
@@ -0,0 +1,13 @@
+package xyz.spikymelon.inked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/spikymelon/inked/InkedApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.spikymelon.inked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
